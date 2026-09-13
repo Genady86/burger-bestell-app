6a99fc2d7db927d650dc31aba4e0aa6c9f021c9e
Genady86 <Genady.Slavo@gmail.com>
2026-09-13 02:57:24 +0200

    Grundlayout und dynamische Burgerliste umgesetzt

--- data.js
+++ data.js
@@ -3,40 +3,40 @@ const dishes = [
         name: 'Big Texas BBQ',
         description: 'Doppeltes Rindfleisch, Bacon, Cheddar, Röstzwiebeln und rauchige BBQ-Sauce',
         price: 16.9,
-        image: './assets/img/big-texas-bbq.jpg',
+        image: './assets/img/big-texas-bbq.webp',
     },
     {
         name: 'New York Smash',
         description:
             'Doppeltes Smash-Patty, Cheddar, Gewürzgurken, Zwiebeln und hausgemachte Burgersauce',
         price: 15.9,
-        image: './assets/img/new-york-smash.jpg',
+        image: './assets/img/new-york-smash.webp',
     },
     {
         name: 'Bacon Cheese Monster',
         description:
             'Doppeltes Rindfleisch, knuspriger Bacon, doppelt Cheddar, Salat und Burgersauce',
         price: 17.9,
-        image: './assets/img/bacon-cheese-monster.jpg',
+        image: './assets/img/bacon-cheese-monster.webp',
     },
     {
         name: 'Hot Chili Burger',
         description:
             'Rindfleisch, Cheddar, Jalapeños, rote Zwiebeln, Salat und scharfe Chili-Sauce',
         price: 15.5,
-        image: './assets/img/hot-chili-burger.jpg',
+        image: './assets/img/hot-chili-burger.webp',
     },
     {
         name: 'Crispy Chicken Deluxe',
         description: 'Knuspriges Hähnchen, Cheddar, Tomate, Salat, Gewürzgurken und würzige Mayo',
         price: 14.9,
-        image: './assets/img/crispy-chicken-deluxe.jpg',
+        image: './assets/img/crispy-chicken-deluxe.webp',
     },
     {
         name: 'Double House Burger',
         description:
             'Doppeltes Rindfleisch, Cheddar, Tomate, Salat, Zwiebeln und Burger-House-Sauce',
         price: 16.5,
-        image: './assets/img/double-house-burger.jpg',
+        image: './assets/img/double-house-burger.webp',
     },
 ];
